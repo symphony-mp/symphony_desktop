d2e06b608d658ca33507e874a2d254f1e6d07021
João Sereia <joao.lo.sereia@gmail.com>
2026-05-17 10:21:24 -0300

    feat(main): start the engine, the UI library, and the route and dependency manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,37 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:symphony/routing/app_routes.dart';
+import 'package:symphony/routing/modules/root_module.dart';
+import 'package:symphony_engine/symphony_engine.dart';
+import 'package:symphony_ui/symphony_ui.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+Future<void> main() async => Future.wait([
+  SymphonyEngine.init(),
+  SymphonyUI.init(ModularApp(module: RootModule(), child: const MyApp())),
+]);
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title, super.key});
-
-  final String title;
-
+class _MyAppState extends State<MyApp> {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void initState() {
+    super.initState();
+    Modular.setInitialRoute(AppRoutes.home);
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+  void dispose() {
+    Modular.dispose();
+    super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) => App(
+    routerConfig: Modular.routerConfig,
+  );
 }
